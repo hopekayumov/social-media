1233641ec37d9957468d6dc8f8e161761d9c7236
HopeKayumov <umidjonkayumov2000@gmail.com>
2022-01-03 17:12:38 +0500

    add Dialogs page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import "./Profile.css";
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 import ProfilePostForm from './ProfilePostForm/ProfilePostForm';
+import ProfilePostItem from './ProfilePostItem/ProfilePostItem';
 
 function Content() {
     return (
         <div className="profile">
             <ProfileInfo />
             <ProfilePostForm />
+            <ProfilePostItem text="My first Post" />
+            <ProfilePostItem text="My second Post" />
         </div>
     )
 }
--- src/components/Profile/ProfilePostItem/ProfilePostItem.jsx
+++ src/components/Profile/ProfilePostItem/ProfilePostItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./ProfilePostItem.css";
+import Avatar from "../../../assets/images/avatar.jpg";
 
-function ProfilePostItem() {
+function ProfilePostItem({text}) {
     return (
-        <div className="post-profile-item">
-            
+        <div className="profile-post-item">
+            <img className="profile-post__avatar" src={Avatar} alt="avatar jpg" />
+            <span className="profile-post__text">{text}</span>
         </div>
     )
 }
